dd8c1f1483d4f285c1f8e206e4c1c217934944ad
jdavis24 <github@nocureforugly.com>
2015-10-01 21:54:03 -0400

    testing form setup at work

--- guipw/guipw.py
+++ guipw/guipw.py
@@ -3,15 +3,19 @@
 from Tkinter import *
 
 root = Tk()
+root.title('gui password test')
 
 label_name = Label(root, text='username:')
 label_pass = Label(root, text='password:')
-entry_name = Entry(root)
-entry_pass = Entry(root)
+entry_name = Entry(root)#, textvariable=namevar)
+entry_pass = Entry(root)#, textvariable=passvar)
 
 label_name.grid(row=0)
 label_pass.grid(row=1)
 entry_name.grid(row=0, column=1)
-entry_name.grid(row=1, column=1)
+entry_pass.grid(row=1, column=1)
+
+# label_displayname = Label(root, textvariable=namevar)
+# label_display.grid(row=2)
 
 root.mainloop()
