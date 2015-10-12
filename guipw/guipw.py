ade3d08f8af8e51e27ff96e3c22eaa2b535a733b
jdavis24 <github@nocureforugly.com>
2015-10-12 18:31:40 -0400

    switched to python3.4, working except print to console

--- guipw/guipw.py
+++ guipw/guipw.py
@@ -1,6 +1,6 @@
-#! /usr/bin/python
+#! /usr/bin/python3
 
-from Tkinter import *
+from tkinter import *
 
 root = Tk()
 root.title('gui password test')
@@ -14,7 +14,7 @@ displayvar = StringVar()
 label_name = Label(root, text='username:')
 label_pass = Label(root, text='password:')
 entry_name = Entry(root, textvariable=namevar)
-entry_pass = Entry(root)#, textvariable=passvar)
+entry_pass = Entry(root, textvariable=passvar)
 label_name.grid(row=0)
 label_pass.grid(row=1)
 entry_name.grid(row=0, column=1)
@@ -22,9 +22,9 @@ entry_pass.grid(row=1, column=1)
 
 # create a label to display entered values for testing
 def display():
-	label_display = Label(root, text='working', fg='blue', textvariable=namevar)
+	label_display = Label(root, text=namevar, fg='blue', textvariable=namevar)
 	label_display.grid(row=3, column=1)
-	print namevar
+	print (namevar)
 	
 # create a button to save
 button_submit = Button(text='Submit', command=display)
